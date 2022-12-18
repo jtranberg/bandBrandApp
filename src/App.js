668f0a4b65d28576430cbfae9bfc55a065ec35a3
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-12-17 21:21:27 -0800

    commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+import logobb from './logobb.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      
+      <header className="App-header">
+        <h1>
+         The Best Band You Can Be
+        </h1>
+        <br>
+        </br>
+        <img src={logobb} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://www.bandbrand.ca/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <br></br>
+         Band Brand
+        </a><h1>
+          All Inclusive Branding For Your Band
+        </h1>
+      </header>
+
+      <div>
+        
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
