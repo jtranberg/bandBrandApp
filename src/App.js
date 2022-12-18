68da87aa0fcbb09da79cae449c6e036f69ee69cd
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-12-17 23:01:11 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -4,34 +4,23 @@ import './App.css';
 
 function App() {
   return (
-    
-    <div className="App">
-      
-      <header className="App-header">
-        <h1>
+    <div className="App"> <h1>
         The BEST BAND you can Be
         </h1>
-        <br>
-        </br>
-        <a href="https://www.bandbrand.ca/"  target="_blank" rel="noopener noreferrer"><img src={logobb} className="App-logo" alt="logo" ></img></a>
-        <a
+        <br></br><br></br>
+        <a href="https://www.bandbrand.ca/"  
+           target="_blank" rel="noopener noreferrer">
+          <img src={logobb} className="App-logo" alt="logo" ></img></a>
+          <br></br><br></br><br></br>
+          <a
           className="App-link"
           href="https://www.bandbrand.ca/"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          <br></br>
-         Band Brand
-        </a><h1>
+          rel="noopener noreferrer">Band Brand</a><h2>
           All Inclusive Branding For Your Band
-        </h1>
-      </header>
-
-      <div>
-        
-
-      </div>
-    </div>
+        </h2><main className="App-header">
+      </main>
+   </div>
   );
 }
 
