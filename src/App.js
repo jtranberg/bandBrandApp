d8069450d26db3b1c2fd73b1ebb369c463689faa
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-12-17 23:28:21 -0800

    adjustments

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="App"> <h1>
-        The BEST BAND you can Be
+        Be the BEST BAND you can Be
         </h1>
-        <br></br><br></br>
+        <br></br>
         <a href="https://www.bandbrand.ca/"  
            target="_blank" rel="noopener noreferrer">
           <img src={logobb} className="App-logo" alt="logo" ></img></a>
-          <br></br><br></br><br></br>
+          <br></br>
           <a
           className="App-link"
           href="https://www.bandbrand.ca/"
           target="_blank"
           rel="noopener noreferrer">Band Brand</a><h2>
+            <br></br>
           All Inclusive Branding For Your Band
-        </h2><main className="App-header">
-      </main>
+        </h2>
+        <br></br><h3>
+            Helping the Talent access the World Wide Network
+          </h3>
+      
    </div>
   );
 }
